a795e624e072c6b5ab67b2ebcc4b80df7f36e1f5
heganjr <jacobhegan@hotmail.com>
2021-03-31 23:43:07 +0800

    added timer started questions array

--- script.js
+++ script.js
@@ -9,3 +9,37 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+let timer = 60;
+let countdownTimer = document.getElementById("countdown");
+let stopTimer;
+
+let Questions = [
+  {
+    Q: "Q1",
+    Choices: ["A: DSJDSJJD", "B:SDHDHSHDSH"],
+    Answer: "A: dsjDSJJASD",
+  },
+  {
+    Q: "Q2",
+    Choices: ["A.ds..sda", "B:JDSHHSDHAHSDA"],
+    Answer: "C:fsdfdsfdfss",
+  },
+];
+// Array for each question - Do a for loop to cycle through the questions each time I click on button
+// Display check answer for a few seconds and then move onto next question
+
+let beginButton = document.getElementById("begin");
+beginButton.addEventListener("click", function () {
+  stopTimer = setInterval(timerStartWhenIClickOnBeginBtn, 1000);
+});
+
+function timerStartWhenIClickOnBeginBtn() {
+  timer--;
+  countdownTimer.textContent = timer;
+  if (timer <= 0) {
+    clearInterval(stopTimer);
+    timer = 0;
+    countdownTimer.textContent = timer;
+  }
+}
