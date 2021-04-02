360ee6bc2c6203c6157b880ab633e1312aaf205a
heganjr <jacobhegan@hotmail.com>
2021-04-02 21:02:21 +0800

    questions arrays added questions added in html

--- script.js
+++ script.js
@@ -14,17 +14,32 @@ let timer = 60;
 let countdownTimer = document.getElementById("countdown");
 let stopTimer;
 
-let Questions = [
+let questions =[
   {
-    Q: "Q1",
-    Choices: ["A: DSJDSJJD", "B:SDHDHSHDSH"],
-    Answer: "A: dsjDSJJASD",
+    Q: "Q1 - Commonly used data types DO NOT include:",
+    Choices: ["A: Strings", "B: Booleans", "C: Alerts", "D: Numbers"],
+    Answer: "C: Alerts",
   },
   {
-    Q: "Q2",
-    Choices: ["A.ds..sda", "B:JDSHHSDHAHSDA"],
-    Answer: "C:fsdfdsfdfss",
+    Q: "Q2 - The condition in an if / else statement is enclosed within ______.",
+    Choices: ["A: Quotes", "B: Curly Brackets", "C: Parentheses", "D: Square Brackets"],
+    Answer: "C: Parentheses",
   },
+  {
+    Q: "Q3 - String values must be enclosed within ______ when being assigned to variables.",
+    Choices: ["A: Commas", "B: Curly Brackets", "C: Quotes", "D: Parentheses"],
+    Answer: "C: Quotes",
+  },
+  {
+    Q: "Q4 - A very useful tool used during development and debugging for printing content to the debugger is:",
+    Choices: ["A: Javascript", "B: Terminal/ Bash", "C: For loops", "D: console.log"],
+    Answer: "D: console.log",
+  },
+  {
+    Q: "Q5 - Arrays in JavaScript can be used to store ______.",
+    Choices: ["A: Numbers and Strings", "B: Other Arrays", "C: Booleans", "D: All of the Above"],
+    Answer: "D: All of the Above",
+  }
 ];
 // Array for each question - Do a for loop to cycle through the questions each time I click on button
 // Display check answer for a few seconds and then move onto next question
